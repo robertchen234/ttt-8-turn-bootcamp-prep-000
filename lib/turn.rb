2de585cc6475c853f3f8cd974d20880e00f2c076
Robert Chen <robertchen234@gmail.com>
2018-10-01 22:36:44 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,6 +15,13 @@ def valid_move?(board, index)
   end
 end
 
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+  end
+end
 
 def move(board, index, "X")
   puts board
