b8bef71313cff094922446a08250581dbb1352a0
Robert Chen <robertchen234@gmail.com>
2018-10-01 23:02:31 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ def turn(board)
   user_input = gets.strip
   input_to_index(user_input)
   if valid_move?(board, index)
-    
+    move(board, index, char="X")
     display_board(board)
   else
     turn(board)
